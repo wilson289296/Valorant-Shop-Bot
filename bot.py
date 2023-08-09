80b1183cbfb0e16bf1a2da59155d174fb1ffc9e0
william su <william@FFR-41MR>
2023-08-09 16:49:55 -0700

    polish

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ def b64enc(str):
 
 def b64dec(str):
     return str
-    return base64.b64decode(str.encode("ascii")).decode("ascii")
+    # return base64.b64decode(str.encode("ascii")).decode("ascii")
 
 # ===================================== /Generic funcs ===================================
 
@@ -61,29 +61,10 @@ async def addaccount(interaction: discord.Interaction, username: str, password: 
     saveCredStore(creds)
     await interaction.response.send_message(f"Successfully added user {interaction.user.nick}", ephemeral=True)
 
-# @bot.tree.command(name="otp")
-# @app_commands.describe(passcode = "Riot one-time passcode")
-# async def addotp(interaction: discord.Interaction, otp: str):
-#     creds = loadCredStore()
-#     if str(interaction.user.id) in creds.keys():
-#         user = creds[str(interaction.user.id)]["username"]
-#         pw = b64dec(creds[str(interaction.user.id)]["password"])
-#         task_queue.append({
-#             "discuser": interaction.user.id,
-#             "user": user,
-#             "pw": pw,
-#             "channel": interaction.channel_id,
-#         })
-#         place = len(task_queue)
-#         global working
-#         if working:
-#             place += 1
-#         await interaction.response.send_message(f"Request queued. You are #{place} in line (est completion in ~{place * 50}s).")
-#     else:
-#         await interaction.response.send_message(f"User not found, try adding yourself first with `/addaccount`.")
-
 @bot.tree.command(name="shop")
 async def get_shop(interaction: discord.Interaction):
+    global response_time_total
+    global jobs
     creds = loadCredStore()
     if str(interaction.user.id) in creds.keys():
         user = creds[str(interaction.user.id)]["username"]
@@ -98,18 +79,25 @@ async def get_shop(interaction: discord.Interaction):
         global working
         if working:
             place += 1
-        await interaction.response.send_message(f"Request queued. You are #{place} in line (est completion in ~{place * 50}s).")
+        try:
+            est_time = response_time_total/jobs
+        except:
+            est_time = 50
+        await interaction.response.send_message(f"Request queued. You are #{place} in line (est completion in ~{est_time:.2f}s).")
     else:
         await interaction.response.send_message(f"User not found, try adding yourself first with `/addaccount`.")
 
-
 task_queue = []
 working = False
+response_time_total = 0
+jobs = 0
 
 @tasks.loop(seconds=0.5)
 async def process_queue():
     global task_queue
     global working
+    global jobs
+    global response_time_total
     # print(f"Number of jobs in queue: {len(task_queue)}")
     if len(task_queue) != 0 and not working:
         print(f"{len(task_queue)} jobs in queue, starting work...")
@@ -121,10 +109,13 @@ async def process_queue():
             print("ss taken, sending photo")
             with open("ss.png", 'rb') as f:
                 picture = discord.File(f)
-                await channel.send(content=f"<@{task['discuser']}> time elapsed = {elap:.2f}s", file=picture)
+                await channel.send(content=f"<@{task['discuser']}>\n*(time elapsed = {elap:.2f}s)*", file=picture)
+            jobs += 1
+            response_time_total += elap
             print(f"completed task for {task['user']}")
         elif result == "otp":
             print("Encountered OTP. Prompting user.")
+            # set flag that user has OTP enabled
             def check(message: discord.Message):
                 return message.channel.id == channel.id and message.author.id == task['discuser'] and len(message.content) == 6
             try:
@@ -139,7 +130,9 @@ async def process_queue():
                     print("ss taken, sending photo")
                     with open("ss.png", 'rb') as f:
                         picture = discord.File(f)
-                        await channel.send(content=f"<@{task['discuser']}> time elapsed = {elap + elap2:.2f}s", file=picture)
+                        await channel.send(content=f"<@{task['discuser']}>\n*(time elapsed = {elap + elap2:.2f}s)*", file=picture)
+                    jobs += 1
+                    response_time_total += elap + elap2
                     print(f"completed task for {task['user']}")
                 else:
                     print("User submitted invalid OTP, cancelling job.")
